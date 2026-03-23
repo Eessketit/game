d2a2c38fa133310caa6887d744edc29ad8f292c5
Shohruz <124710827+Eessketit@users.noreply.github.com>
2026-03-23 15:55:57 +0500

    fix

--- Program.cs
+++ Program.cs
@@ -4,33 +4,37 @@ using TicTacToeApp.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Render assigns a dynamic port via the 'PORT' environment variable
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://*:{port}");
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// It automatically retrieves 'DefaultConnection' from appsettings.json or 'ConnectionStrings__DefaultConnection' Environment Variable
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") 
+    ?? Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection") 
+    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+if (!string.IsNullOrEmpty(connectionString) && (connectionString.StartsWith("postgres://") || connectionString.StartsWith("postgresql://")))
+{
+    var uri = new Uri(connectionString);
+    var userInfo = uri.UserInfo.Split(':');
+    var password = userInfo.Length > 1 ? userInfo[1] : "";
+    connectionString = $"Host={uri.Host};Port={(uri.Port > 0 ? uri.Port : 5432)};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={password};Ssl Mode=Require;Trust Server Certificate=true;";
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-    
+    options.UseNpgsql(connectionString));
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-// Automatically apply database migrations on startup
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
